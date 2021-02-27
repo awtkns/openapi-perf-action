4a13702dea76c16b63857f1ecdbf750e8f43f87d
Adam <cadamrun@gmail.com>
2021-02-27 10:00:47 -0800

    :sparkles: on pr comment

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from os import environ
 
 OPEN_API_ENDPOINT = environ.get('INPUT_OPENAPI-ENDPOINT', "Could not find endpoint")
 
-print(environ)
-print(OPEN_API_ENDPOINT)
+if __name__ == '__main__':
+    print(environ)
+    print(OPEN_API_ENDPOINT)
