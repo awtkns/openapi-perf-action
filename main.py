4b4a61ce5546914f121cd705848429c3dc677c70
Adam <cadamrun@gmail.com>
2021-02-27 10:11:15 -0800

    :sparkles: github event file

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import json
+
 from os import environ
 
 OPEN_API_ENDPOINT = environ.get('INPUT_OPENAPI-ENDPOINT', "Could not find endpoint")
+GITHUB_EVENT_PATH = environ.get('GITHUB_EVENT_PATH')
+
+with open(GITHUB_EVENT_PATH) as fp:
+    print(json.load(fp))
 
 print(environ)
 print(OPEN_API_ENDPOINT)
