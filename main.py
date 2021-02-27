3323dc75ce09973fb24006571e1add7cc1284b00
Adam <cadamrun@gmail.com>
2021-02-27 09:47:02 -0800

    :construction: no need for tagging now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import environ
 
-OPEN_API_ENDPOINT = environ.get('INPUT_OPEN-ENDPOINT', "Could not find endpoint")
+OPEN_API_ENDPOINT = environ.get('INPUT_OPENAPI-ENDPOINT', "Could not find endpoint")
 
+print(environ)
 print(OPEN_API_ENDPOINT)
