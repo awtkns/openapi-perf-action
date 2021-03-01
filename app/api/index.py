ee8a9e6c405728ea9c8026ecd401d968c474f044
Adam <cadamrun@gmail.com>
2021-02-28 20:42:56 -0800

    :bug: GITHUB key unreadable

--- app/api/index.py
+++ app/api/index.py
@@ -1,9 +1,31 @@
-from pydantic import BaseModel
+import os
+
 from fastapi import FastAPI, HTTPException
+from github3.exceptions import NotFoundError, ForbiddenError
+from github3.github import GitHub
+from pydantic import BaseModel
 
-from _github import login_as_installation
+GITHUB_PRIVATE_KEY = os.environ.get('APP_PRIVATE_KEY', None)
+GITHUB_APP_IDENTIFIER = os.environ.get('APP_IDENTIFIER', None)
 
-app = FastAPI()
+if not GITHUB_PRIVATE_KEY:
+    GITHUB_PRIVATE_KEY = open('private-key.pem', 'rt').read()
+
+
+def login_as_installation(account: str):
+    gh = GitHub()
+    gh.login_as_app(GITHUB_PRIVATE_KEY.encode(), GITHUB_APP_IDENTIFIER)
+
+    for install in gh.app_installations():
+        if install.account['login'] == account:
+            gh.login_as_app_installation(
+                GITHUB_PRIVATE_KEY.encode(),
+                GITHUB_APP_IDENTIFIER,
+                install.id
+            )
+            return gh
+
+    raise HTTPException(status_code=403, detail="App Installation not found")
 
 
 class ActionIn(BaseModel):
@@ -15,6 +37,9 @@ class ActionIn(BaseModel):
     @property
     def repo(self) -> str:
         return f'{self.owner}/{self.repository}'
+
+
+app = FastAPI()
 
 
 @app.post('/api/')
