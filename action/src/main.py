36de0c9c68cf615f1a2ae96f2d75e43f32e65f5b
Adam Watkins <cadamrun@gmail.com>
2021-04-18 21:37:22 -0700

    :fire: Updating Action to work with openapi-perf

--- action/src/main.py
+++ action/src/main.py
@@ -3,11 +3,10 @@ from io import BytesIO
 from os import environ
 from uuid import uuid4
 
-import numpy as np
 import requests as r
-from matplotlib import pyplot as plt
 
 from firebase import firestore
+from openapi_perf import OpenAPIPerf
 
 OPEN_API_ENDPOINT = environ.get('INPUT_OPENAPI-ENDPOINT', "Could not find endpoint")
 APP_ENDPOINT = 'https://api.openapi-perf.awtkns.com'
@@ -24,11 +23,10 @@ with open(environ.get('GITHUB_EVENT_PATH')) as fp:
     GITHUB_EVENT = json.load(fp)
 
 
-def fig_to_base64():
+def fig_to_base64(figure):
     plt_bytes = BytesIO()
-    plt.savefig(plt_bytes, format='png')
+    figure.savefig(plt_bytes, format='png')
     plt_bytes.seek(0)
-    plt.close()
 
     return plt_bytes
 
@@ -47,30 +45,18 @@ if __name__ == '__main__':
         url=f'{APP_ENDPOINT}/reaction',
         json=action_factory('eyes')
     )
-    assert res.status_code == 200, "Could not React to action " + action
+    assert res.status_code == 200, "Could not React to action"
 
-    x1 = np.linspace(0.0, 5.0)
-    x2 = np.linspace(0.0, 2.0)
+    op = OpenAPIPerf(APP_ENDPOINT)
+    results = op.run()
 
-    y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
-    y2 = np.cos(2 * np.pi * x2)
-
-    plt.subplot(2, 1, 1)
-    plt.plot(x1, y1, 'o-')
-    plt.title('A tale of 2 subplots')
-    plt.ylabel('Damped oscillation')
-
-    plt.subplot(2, 1, 2)
-    plt.plot(x2, y2, '.-')
-    plt.xlabel('time (s)')
-    plt.ylabel('Undamped')
-
-    file = fig_to_base64()
+    fig = results.plot(show=False)
+    file = fig_to_base64(fig)
 
     url = f'charts/{uuid4().hex}'
     img = firestore.child(url).put(file)
 
-    comment = f'Performance Report\n---\nFrom a serverless function!\n<p align="center"><img src="{firestore.child(url).get_url("")}"></p>'
+    comment = f'<p align="center"><img src="{firestore.child(url).get_url("")}"></p>'
     res = r.post(
         url=f'{APP_ENDPOINT}/comment',
         json=action_factory(comment)
